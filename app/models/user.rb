0b8a501c7b6068008d838ef22ae20e68fe263047
Zac Willmington <zac.willmington@gmail.com>
2018-07-12 23:06:32 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
-
-  def user_attributes=(attributes)
-      binding.pry
-      if attributes[:username] != ""
-          User.find_or_create_by(attributes[:username])
-      end
-  end
 end
